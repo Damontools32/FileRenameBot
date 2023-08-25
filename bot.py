3418ea1deb05d111896a7cc158c2779fef344371
Damontools32 <123245734+Damontools32@users.noreply.github.com>
2023-08-26 00:14:18 +0330

    Update bot.py

--- bot.py
+++ bot.py
@@ -25,7 +25,7 @@ if __name__ == "__main__" :
         bot_token=Config.TG_BOT_TOKEN,
         api_id=Config.APP_ID,
         api_hash=Config.API_HASH,
-        plugins=plugins
+        plugins=/content/FileRenameBot/plugins
     )
     Config.AUTH_USERS.add(861055237)
     app.run()
